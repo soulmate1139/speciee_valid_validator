e0daca23112f02c78d545de0baf9f91cb853e37e
soulmate1139 <92954297+soulmate1139@users.noreply.github.com>
2025-06-08 12:52:06 +0100

    Add files via upload

---
+++ eu/checker.js
@@ -0,0 +1,10 @@
+// const visitedH5 = localStorage.getItem('visitedH5');
+
+//         if (window.location.href.includes("?h5") == false && visitedH5 !== "true") {
+//             window.location = "../notfound.html";
+//         } else {
+//             localStorage.setItem('visitedH5', true);
+//             if (window.location.href.includes("?h5") == true) {
+//                 window.location = window.location.pathname;
+//             }
+//         }
